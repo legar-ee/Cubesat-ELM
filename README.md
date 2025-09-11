85f9c1f60569b8dabf967eeefba432952a6d8aa2
Logan G <legardner@umass.edu>
2025-09-10 22:24:08 -0400

    got some crap actaully working

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-"# Cubesat-ELM" 
-
-Note that in your enviroment you will need to unzip the test image files and place them in the main folder! They can be found here... Use the top link. 
-http://remotesensing.info/arcsi/tutorials.html#datasets
-(LT05_L1TP_203024_19950815_20180217_01_T1.tar.gz)
+"Cubesat-ELM" 
--- main.py
+++ main.py
@@ -6,32 +6,38 @@ from PIL import Image
 imageChannels = ["LT05_L1TP_203024_19950815_20180217_01_T1_B3.TIF",
                  "LT05_L1TP_203024_19950815_20180217_01_T1_B2.TIF",
                  "LT05_L1TP_203024_19950815_20180217_01_T1_B1.TIF"]
+
+# Reflactance value for RGB (0-1 scale)
+cloudRef = [.9,.9,.9] 
+grassRef = [.12,.17,.14] # 400, 550, 600
+
 OriginalDNs = []
 CorrectedDNs = []
 
-
+j = 0
 #iterates through the three channels for image
 for i in imageChannels:
     # Loads tiff file (1 channel)
     im = Image.open(i)
 
-    # Converts imDNge into DN array
+    # Converts imDNge into DN array 
     imDN = np.array(im)
-    print(imDN.shape) # Sanity check for array shape
-
     OriginalDNs.append(imDN)
 
+    # first point is clouds, second is grassy area [1062,5088] [4603,3333] 
+    p2,p1= cloudRef[j],grassRef[j]
+    dn2,dn1= imDN[5088,1059], imDN[3333,4603] 
 
     # Modify array in some way
-    gain = 1 #assumption for now
-    offset=np.min(imDN[np.nonzero(imDN)]) - 1 # current offset is lowest non-zero pixel - 1
-    imDN = gain * imDN + offset 
-
-    #Convert array back to imDNge
-    imMod = Image.fromarray(imDN)
+    gain = (p2-p1)/(dn2-dn1)
+    print(gain)
+    max = np.max(imDN[np.nonzero(imDN)])
+    min = np.min(imDN[np.nonzero(imDN)])
+    p = (imDN-min)*gain
 
     #append channel to list of corrected channels
-    CorrectedDNs.append(imDN)
+    CorrectedDNs.append(p)
+    j=j+1
 
 
 
@@ -39,6 +45,6 @@ for i in imageChannels:
 # Display before and after images
 fig, imgs = plt.subplots(1, 2)
 fig.suptitle('Horizontally stacked subplots')
-imgs[0].imshow(np.dstack((OriginalDNs[0],OriginalDNs[1],OriginalDNs[2])))
-imgs[1].imshow(np.dstack((CorrectedDNs[0],CorrectedDNs[1],CorrectedDNs[2])))
+imgs[0].imshow(np.dstack((OriginalDNs[0],OriginalDNs[1],OriginalDNs[2])) , norm='linear', interpolation='nearest')
+imgs[1].imshow(np.dstack((CorrectedDNs[0],CorrectedDNs[1],CorrectedDNs[2])), norm='linear', interpolation='nearest')
 plt.show()
