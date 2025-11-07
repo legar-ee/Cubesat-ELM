1c6e18f12738dc7b4c1e616d7e8a76784a0b8b2a
Tuyen Dau <tdau@umass.edu>
2025-11-07 12:49:09 -0500

    update functions to find target radiance values and compute ELM

--- ELM.py
+++ ELM.py
@@ -1,24 +1,39 @@
 import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.stats import linregress
 
 
-def dark_bright(image):  # Compute darkest/brightest radiances and their pixels in each band
+def target_radiance(image, known_endmember): # Find the same-type (grass, cloud, etc) radiance values as the known endmembers
+    # Argument: image = 3D array (row, col, band); known_endmember = 1D array for each type (grass/cloud/etc)
+
     row, col, band = image.shape
 
-    darkest_rads = np.min(image, axis=(0, 1))
-    brightest_rads = np.max(image, axis=(0, 1))
-    
-    darkest_pixels = np.argmin(image, axis=(0, 1))
-    brightest_pixels = np.argmax(image, axis=(0, 1))
-    darkest_row, darkest_col = np.unravel_index(darkest_pixels, (row, col))
-    brightest_row, brightest_col = np.unravel_index(brightest_pixels, (row, col))
-    darkest_pixels = list(zip(darkest_row, darkest_col))
-    brightest_pixels = list(zip(brightest_row, brightest_col))
+    # Find the radiance values in the image
+    image_flat = image.reshape((row*col), band)
+    known_endmember = known_endmember.reshape(1, band)
+    match_value = cosine_similarity(image_flat, known_endmember).flatten()
+    target_index = np.argmax(match_value)
+    target_row, target_col = np.unravel_index(target_index, (row, col))
 
-    # Rads are in an 1D array with 'band' number of elements, and pixels are a list of 'band' number of (row, col) tuples
-    return (darkest_rads, darkest_pixels, brightest_rads, brightest_pixels)
+    return image[target_row, target_col] # Return an 1D array of each type with 'band' elements; will need to stack them all up later
 
 
-def elm(image, dark_bright_data):
-    
+def elm(image, target_radiance, known_endmember): # Compute ELM
+    # Argument: image = 3D array (row, col, band); target_radiance = 2D array (number of radiance values, band); kn
+    row, col, band = image.shape
+    gain = np.zeros(band)
+    offset = np.zeros(band)
+
+    for i in range(band):
+        slope, intercept, _, _, _ = linregress(target_radiance[:, i], known_endmember[:, i])
+
+        gain[i] = slope
+        offset[i] = intercept
+
+    gain = gain[np.newaxis, np.newaxis, :]
+    offset = offset[np.newaxis, np.newaxis, :]
+
+    return image*gain + offset
+
 
 
